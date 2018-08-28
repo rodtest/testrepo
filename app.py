1297d175cc5f2aa37ce3e76e0749d8b86911b6f4
rodcoelho <rodcpython@gmail.com>
2018-08-28 11:12:58 -0400

    test

--- app.py
+++ app.py
@@ -5,9 +5,7 @@ from time import time
 repo_dir = ''  # path to files
 repo = Repo(repo_dir)
 file_list = [  # list of files to updates in repo_dir
-    'app.py',
-    'app2.py',
-    'app3.py'
+    'pushing.json'
 ]
 commit_message = 'Updated {}'.format(time())  # commit message logs unix time
 repo.index.add(file_list)
